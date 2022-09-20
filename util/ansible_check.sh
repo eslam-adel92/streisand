56a07c37e9051ef64196e90be6b5847228f90729
Eslam Adel <prog.eslamadel@gmail.com>
2022-09-20 18:12:46 +0200

    fix the ansible checker condition.

--- util/ansible_check.sh
+++ util/ansible_check.sh
@@ -5,7 +5,7 @@
 # check_ansible checks that Ansible is installed on the local system
 # and that it is a supported version.
 function check_ansible() {
-  local REQUIRED_ANSIBLE_VERSION="2.0"
+  local REQUIRED_ANSIBLE_VERSION="2.0.0"
 
   if ! command -v ansible > /dev/null 2>&1; then
     echo "
